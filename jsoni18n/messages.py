62849fc83c6e86135c13d6036390fc0998d927e9
Pycryptor10 <47485078+Pycryptor10@users.noreply.github.com>
2021-05-02 11:15:37 +0100

    Update messages.py

--- jsoni18n/messages.py
+++ jsoni18n/messages.py
@@ -17,10 +17,10 @@ def get_messages(language, messagefiles):
     Raises:
         ValueError: 'language' parameter does not match a valid language with a file.
     """
-    availableLangCodes = get_available_languages(messagefiles)
+    available_lang_codes = get_available_languages(messagefiles)
     with open(messagefiles.format('eng'), 'r') as messagefileeng:
         messages = loads(messagefileeng.read())
-        if language in availableLangCodes:
+        if language in available_lang_codes:
             if language != 'eng':
                 with open(messagefiles.format(language), 'r') as messagefilelangs:
                     filedata = loads(messagefilelangs.read())
