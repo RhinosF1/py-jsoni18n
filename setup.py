40104f1f88fff4e87c7844f959a4ed047f79ae1f
Pycryptor10 <47485078+Pycryptor10@users.noreply.github.com>
2021-05-02 12:38:33 +0100

    Create setup.cfg (#6)
    * Create setup.cfg
    
    * add maintainer too
    
    * Update setup.py
    
    * Update setup.py
    
    * Update __init__.py
    
    * Update setup.py
    
    * Update setup.cfg
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -14,16 +14,22 @@ with open('requirements.txt') as requirements_file:
 with open('dev-requirements.txt') as dev_requirements_file:
     dev_requirements = list(dev_requirements_file.readlines())
 
+username = 'Pycryptor10'
+email = 'Pycryptor10@gmail.com'
 
 setup(
     name='jsoni18n',
     version='0.0.1',
     url='https://github.com/Pycryptor10/py-jsoni18n',
     description='A simple i18n system using json + pycountry alpha_3 ISO codes.',
+    keywords='pycountry localization json i18n l10n internationalization',
     long_description=readme,  # + '\n\n' + history,
     long_description_content_type='text/markdown',  # This is important!
-    author='Pycryptor10',
-    author_email='Pycryptor10@gmail.com',
+    author=username,
+    author_email=email,
+    maintainer=username,
+    maintainer_email=email,
+    platforms='any',
     packages=find_packages('.'),
     include_package_data=True,
     install_requires=requirements,
