e59f79efad626cf29de262666b6c2eb03059c11f
Pycryptor10 <Pycryptor10@gmail.com>
2021-05-02 11:09:46 +0100

    + docs

--- jsoni18n/languages.py
+++ jsoni18n/languages.py
@@ -2,16 +2,29 @@
 from pycountry import languages
 
 
-def getLangDict():
+def get_lang_dict():
+    """Get a dict with code: name for each language that is in pycountry.
+
+    Returns:
+        dict: code: name for each alpha_3 code in pycountry
+    """
     langdict = {}
     for lang in languages:
         langdict[lang.alpha_3] = lang.name
     return langdict
 
 
-def getAvailableLanguages(messageLocation):
+def get_available_languages(messageLocation):
+    """Get a dict with code: name for each language that is in pycountry.
+
+    Args:
+        messageLocation (str): path to messages.  Example: /path/{}.json
+
+    Returns:
+        list: list of valid lanuages that have a file in messagelocation.
+    """
     availableLanguages = []
-    for langcode in getLangDict():
+    for langcode in get_lang_dict():
         try:
             open(messageLocation.format(langcode), 'r')
             availableLanguages.append(langcode)
