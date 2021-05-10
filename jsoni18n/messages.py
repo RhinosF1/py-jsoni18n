371291f1b6000b41c85db78e9351976c53fdebc7
Pycryptor10 <47485078+Pycryptor10@users.noreply.github.com>
2021-05-10 14:34:56 +0100

    Release 0.2.0 (#16)
    * Bump pytest from 6.2.3 to 6.2.4 (#11)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Pycryptor10 <Pycryptor10@gmail.com>
    
    * Bump flake8 from 3.9.1 to 3.9.2 (#14)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump setuptools from 56.0.0 to 56.2.0 (#12 & #13)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * messages: add fallback/format param (#15)
    * bump version
    * Create CODE_OF_CONDUCT.md
    * Create SECURITY.md
    * Update meta files
    * fix CI
    
    
    Co-authored-by: DeepSource Bot <bot@deepsource.io>
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>
    Co-authored-by: Pycryptor10 <Pycryptor10@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- jsoni18n/messages.py
+++ jsoni18n/messages.py
@@ -4,12 +4,14 @@ from json import loads
 from jsoni18n.languages import get_available_languages
 
 
-def get_messages(language, messagefiles):
+def get_messages(language, messagefiles, fallback='eng', fileformat='json'):
     """Get a dict with message-name: value for a language based on file. Default to english version if missing.
 
     Args:
         language (str): language to load
-        messagefiles (str): path to messages.  Example: /path/{}.json
+        messagefiles (str): path to messages.  Example: /path/{}
+        fallback (str): fallback language if requested is missing messages. Default: 'eng'
+        fileformat (str): filetype. Default: json
 
     Returns:
         dict: message-name: value for each detected message
@@ -17,12 +19,13 @@ def get_messages(language, messagefiles):
     Raises:
         ValueError: 'language' parameter does not match a valid language with a file.
     """
-    available_lang_codes = get_available_languages(messagefiles)
-    with open(messagefiles.format('eng'), 'r') as messagefileeng:
+    available_lang_codes = get_available_languages(
+        messagefiles, fileformat=fileformat)
+    with open(f'{messagefiles.format(fallback)}.{fileformat}', 'r') as messagefileeng:
         messages = loads(messagefileeng.read())
         if language in available_lang_codes:
-            if language != 'eng':
-                with open(messagefiles.format(language), 'r') as messagefilelangs:
+            if language != fallback:
+                with open(f'{messagefiles.format(language)}.{fileformat}', 'r') as messagefilelangs:
                     filedata = loads(messagefilelangs.read())
                     for message in filedata.keys():
                         messages[message] = filedata[message]
