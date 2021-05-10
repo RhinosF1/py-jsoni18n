fe7cd5af23a3d87837181911a8ed6d7237b9c57b
Pycryptor10 <47485078+Pycryptor10@users.noreply.github.com>
2021-05-10 12:00:49 +0100

    Update messages.py

--- jsoni18n/messages.py
+++ jsoni18n/messages.py
@@ -4,14 +4,14 @@ from json import loads
 from jsoni18n.languages import get_available_languages
 
 
-def get_messages(language, messagefiles, fallback='eng', format='json'):
+def get_messages(language, messagefiles, fallback='eng', fileformat='json'):
     """Get a dict with message-name: value for a language based on file. Default to english version if missing.
 
     Args:
         language (str): language to load
         messagefiles (str): path to messages.  Example: /path/{}
         fallback (str): fallback language if requested is missing messages. Default: 'eng'
-        format (str): filetype. Default: json
+        fileformat (str): filetype. Default: json
 
     Returns:
         dict: message-name: value for each detected message
@@ -19,12 +19,12 @@ def get_messages(language, messagefiles, fallback='eng', format='json'):
     Raises:
         ValueError: 'language' parameter does not match a valid language with a file.
     """
-    available_lang_codes = get_available_languages(messagefiles, format=format)
-    with open(f'{messagefiles.format(fallback)}.{format}', 'r') as messagefileeng:
+    available_lang_codes = get_available_languages(messagefiles, fileformat=fileformat)
+    with open(f'{messagefiles.format(fallback)}.{fileformat}', 'r') as messagefileeng:
         messages = loads(messagefileeng.read())
         if language in available_lang_codes:
             if language != fallback:
-                with open(f'{messagefiles.format(fallback)}.{format}', 'r') as messagefilelangs:
+                with open(f'{messagefiles.format(fallback)}.{fileformat}', 'r') as messagefilelangs:
                     filedata = loads(messagefilelangs.read())
                     for message in filedata.keys():
                         messages[message] = filedata[message]
