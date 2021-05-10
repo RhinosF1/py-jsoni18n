eb82aad87ea3a551007dac93b8f75af2b65b27bb
Pycryptor10 <47485078+Pycryptor10@users.noreply.github.com>
2021-05-10 16:10:42 +0100

    languages: fix len

--- jsoni18n/languages.py
+++ jsoni18n/languages.py
@@ -1,6 +1,5 @@
 """Get information on langauges from pycountry."""
 from glob import glob
-from os import getcwd
 from warnings import warn
 
 from pycountry import languages
@@ -32,15 +31,16 @@ def get_available_languages(message_location, fileformat='json'):
         ValueError: No languages could be found
     """
     available_languages = []
-    pattern = f'{getcwd()}/{message_location.rstrip("/")}/*.{fileformat}'
+    pattern = f'{message_location.rstrip("/")}/*.{fileformat}'
     for name in glob(pattern, recursive=True):
-        lang = name[:-(len(fileformat))
-                    ][len(f'{getcwd()}/{message_location.rstrip("/")}'):]
+        print(f'Checking {name}')
+        lang = name[:-(len(fileformat)+1)][len(message_location.rstrip('/'))+1:]
+        print(lang)
         if lang in get_lang_dict():
             available_languages.append(lang)
         else:
             warn(
-                f'{lang} was found at {name} but is not a valid language', ResourceWarning)
+                f'{lang} was found at {name} but is not a valid language', Warning)
     if available_languages == []:
         raise ValueError(
             f'No languages were found matching: {pattern}. Did you specify the right location and format?')
