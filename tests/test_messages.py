38203f2982fef8c9577a13ebdcd5108882f59ce8
Pycryptor10 <47485078+Pycryptor10@users.noreply.github.com>
2021-05-17 15:23:59 +0100

    Update test_messages.py

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -61,19 +61,19 @@ def test_fails_on_lang_invalid_and_no_message(get_message_location):
     assert failed
 
 
-def test_base_english(get_message_location_trail):
+def test_base_english_trailing(get_message_location_trail):
     """Test collection of english file."""  # noqa: DAR101, DAR201
     messages = get_messages('eng', get_message_location)
     assert messages == {"message1": "testmessageforstart", "hello": "hello"}
 
 
-def test_messages_french(get_message_location_trail):
+def test_messages_french_trailing(get_message_location_trail):
     """Test collection of french override file."""   # noqa: DAR101, DAR201
     messages = get_messages('fra', get_message_location)
     assert messages == {"message1": "testmessageforstart", "hello": "salut"}
 
 
-def test_fails_on_non_existent(get_message_location_trail):
+def test_fails_on_non_existent_trailing(get_message_location_trail):
     """Test correct failure on non existent file."""   # noqa: DAR101, DAR201
     failed = False
     try:
@@ -84,7 +84,7 @@ def test_fails_on_non_existent(get_message_location_trail):
     assert failed
 
 
-def test_fails_on_lang_invalid_but_message_exists(get_message_location_trail):
+def test_fails_on_lang_invalid_but_message_exists_trailing(get_message_location_trail):
     """Test correct failure on invalid language with messages."""  # noqa: DAR101, DAR201
     failed = False
     try:
@@ -95,7 +95,7 @@ def test_fails_on_lang_invalid_but_message_exists(get_message_location_trail):
     assert failed
 
 
-def test_fails_on_lang_invalid_and_no_message(get_message_location_trail):
+def test_fails_on_lang_invalid_and_no_message_trailing(get_message_location_trail):
     """Test correct failure on invalid language with no messages."""  # noqa: DAR101, DAR201
     failed = False
     try:
